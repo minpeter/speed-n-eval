334e7a171682169d68f654eb061158aaeb2069ed
minpeter <minpeter@friendli.ai>
2024-12-16 21:27:38 +0900

    implement model evaluation with AI generation and enhance CSV data processing

--- eval.ts
+++ eval.ts
@@ -1,11 +1,60 @@
+import { generateObject, generateText } from "ai";
 import { loadCSV } from "./csv";
+import { friendli } from "@friendliai/ai-provider";
+
+const evalModel = async (
+  question: string,
+  choices: string[],
+  answer: string
+) => {
+  const longest = choices.reduce((prev, curr) =>
+    prev.length > curr.length ? prev : curr
+  );
+
+  try {
+    const { object } = await generateObject({
+      model: friendli("meta-llama-3.1-70b-instruct"),
+      maxTokens: longest.length + 50,
+      output: "enum",
+      enum: choices,
+      prompt: question,
+    });
+
+    return object === answer;
+  } catch {
+    return false;
+  }
+};
 
 async function main() {
   try {
     const data = await loadCSV("data/mmlu.csv");
-    console.log(data);
+    let correct = 0;
+    let wrong = 0;
+    let total = 0;
+
+    const results = await Promise.all(
+      data
+        .slice(-5)
+        .map(({ Question, A, B, C, D, Answer }) =>
+          evalModel(Question, [A, B, C, D], Answer)
+        )
+    );
+
+    results.forEach((result) => {
+      if (result) {
+        correct++;
+      } else {
+        wrong++;
+      }
+      total++;
+    });
+
+    console.log(`Correct: ${correct}`);
+    console.log(`Wrong: ${wrong}`);
+    console.log(`Total: ${total}`);
   } catch (error) {
-    console.error("Error loading CSV:", error);
+    console.error("Error:", error);
   }
 }
 
